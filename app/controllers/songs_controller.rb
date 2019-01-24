67bc98f1dc91f66576366381be244c93379ddf31
Gilbert Torchon <gilbert@noukod.com>
2019-01-24 17:55:25 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,8 +12,9 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.new(song_params)
-
+    @song = Song.new(params[:song])
+    puts params.inspect
+    puts song_params.inspect
     if @song.save
       redirect_to @song
     else
@@ -47,7 +48,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title,:artist_name,:genre_name,:note_contents)
   end
 end
-
