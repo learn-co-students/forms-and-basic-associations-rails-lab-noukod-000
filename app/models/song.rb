2d5e637e5bbbc5d61c5fa08ebc3fd4ae964001b5
Jean Gérard Bousiquot <jgb@noukod.com>
2019-02-09 23:17:28 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,30 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+    def artist_name=(name)
+     self.artist = Artist.find_or_create_by(name: name)
+    end
+
+    def artist_name
+      self.artist ? self.artist.name : nil
+    end
+
+    def genre_name=(name)
+     self.genre = Genre.find_or_create_by(name: name)
+    end
+
+    def genre_name
+      self.genre ? self.genre.name : nil
+    end
+
+    def note_contents= contents
+     contents.each do |note_content|
+       self.notes.build(content: note_content);
+     end
+    end
+
+   def note_contents
+     self.notes.map { |note| note.content }.reject { |note| note.empty? }
+   end
+end
