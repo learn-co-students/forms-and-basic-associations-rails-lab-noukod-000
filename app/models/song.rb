7088d625dac4f16b10324b8243081745555c5f8c
Herna Michel <labellehernamichel@yahoo.fr>
2019-07-09 14:44:52 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,26 +3,56 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
-  def artist=(name)
-    self.artist_id = Artist.find_or_create_by(name: name).id
-  end
+  def genre_name=(name)
+  genre = Genre.find_or_create_by(name: name)
+  self.genre = genre
+end
 
-   def song_notes=(notes)
-    notes.each do |note|
-      self.notes.build(content: note)
+def genre_name
+  self.try(:genre).try(:name)
+end
+
+def artist_name
+  self.try(:artist).try(:name)
+end
+
+def artist_name=(name)
+  artist = Artist.find_or_create_by(name: name)
+  self.artist = artist
+end
+
+def note_contents=(notes)
+  notes.each do |content|
+    if content.strip != ''
+      self.notes.build(content: content)
     end
   end
+end
 
-   def genre=(id)
-    self.genre_id = id.to_i
-  end
+def note_contents
+  self.notes.map(&:content)
+end
 
-   def note_contents
-    content = []
-    self.notes.each do |note|
-      content << note.content
-    end
-    content
-  end
+  # def artist=(name)
+  #   self.artist_id = Artist.find_or_create_by(name: name).id
+  # end
+  #
+  #  def song_notes=(notes)
+  #   notes.each do |note|
+  #     self.notes.build(content: note)
+  #   end
+  # end
+  #
+  #  def genre=(id)
+  #   self.genre_id = id.to_i
+  # end
+  #
+  #  def note_contents
+  #   content = []
+  #   self.notes.each do |note|
+  #     content << note.content
+  #   end
+  #   content
+  # end
 
 end
