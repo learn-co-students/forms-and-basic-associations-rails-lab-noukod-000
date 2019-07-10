2026c3fe62d19da8fc9e531826ef73dacbbb47ba
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-07-10 14:11:38 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,34 +3,33 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
-  def genre_name=(name)
-      genre = Genre.find_or_create_by(name: name)
-      self.genre = genre
-    end
+  def genre_name= name
+    #since the song has no genre, we have to create it
+    self.genre = Genre.find_or_create_by name: name
+  end
 
-    def genre_name
-      self.try(:genre).try(:name)
-    end
+  def genre_name
+    #if the song has a genre return the genre's name else return nil
+    genre ? genre.name : nil
+  end
 
-    def artist_name
-      self.try(:artist).try(:name)
-    end
+  def artist_name= name
+    self.artist = Artist.find_or_create_by name: name
+  end
 
-    def artist_name=(name)
-      artist = Artist.find_or_create_by(name: name)
-      self.artist = artist
-    end
+  def artist_name
+    artist ? artist.name : nil
+  end
 
-    def note_contents=(notes)
-      notes.each do |content|
-        if content.strip != ''
-          self.notes.build(content: content)
-        end
-      end
+  def note_contents= contents
+    contents.each do |c|
+      self.notes << Note.find_or_create_by(content: c) unless c.empty?
     end
+  end
 
-    def note_contents
-      self.notes.map(&:content)
+  def note_contents
+    notes.map do |n|
+      n.content
     end
-
+  end
 end
